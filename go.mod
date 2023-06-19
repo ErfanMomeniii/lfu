8182358d03b9d89123e844e9d2263ef01c537838
Erfan Momeni <Erfan.Momeni@snapp.cab>
2023-06-19 19:19:14 +0330

    feat: add some logics

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/erfanmomeniii/lfu
+
+go 1.20
+
+require github.com/erfanmomeniii/heap v1.0.0
---
+++ lfu.go
@@ -0,0 +1,60 @@
+package lfu
+
+import "github.com/erfanmomeniii/heap"
+
+const DefaultSize int64 = 2000
+
+type Lfu struct {
+	values    map[any]any
+	frequents map[any]int
+	minHeap   *heap.MinHeap
+	size      int64
+}
+
+// Get returns the value associated with the inputted key.
+func (l *Lfu) Get(key any) any {
+	return l.values[key]
+}
+
+// purge removes the least used element from cache.
+func (l *Lfu) purge() {
+	if len(l.values) > int(l.size) {
+		_, key := l.minHeap.GetMin()
+		l.minHeap.Delete()
+		delete(l.values, key)
+		delete(l.frequents, key)
+	}
+
+	return
+}
+
+// Set adds or updates with inputted key and value.
+func (l *Lfu) Set(key any, value any) {
+	if _, ok := l.values[key]; ok {
+		// TODO update heap
+		l.values[key] = value
+		l.frequents[key]++
+	} else {
+		l.minHeap.Insert(1, key)
+		l.values[key] = value
+		l.frequents[key] = 1
+	}
+
+	l.purge()
+	return
+}
+
+// Has checks whether the key exists or not.
+func (l *Lfu) Has(key any) bool {
+	_, ok := l.values[key]
+
+	return ok
+}
+
+func New(size ...int64) *Lfu {
+	s := DefaultSize
+	if len(size) > 0 {
+		s = size[0]
+	}
+	return &Lfu{values: make(map[any]any), frequents: make(map[any]int), minHeap: heap.NewMin(), size: s}
+}
